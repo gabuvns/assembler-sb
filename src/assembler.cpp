7311fa49baa43b1c8970041b23903575cdc865b7
Gabriel Soares <cgvilasnovas@gmail.com>
2021-03-08 11:27:24 -0300

    Fix: correctly reads commentary

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -12,29 +12,36 @@ using std::ifstream;
 using std::string;
 using std::vector;
 int lineCounter = 0;
+vector<vector<string>> currentProgram;
 
 int isInvalidChar(char character, int indexCounter) {
     // Checks if symbol starts with number
     // Throw codes: 0->invalid char, 1->
     try{
+        // Checks if symbol starts with number
         if(indexCounter == 0 && (int)character >=48 && (int)character <=57){
             throw(1);
+        }
+        // Checks for invalid char
+        else if(!((int) character >= 65 && (int)character <= 90) && !((int)character >=48 && (int)character <=57)) {
+            cout << "Valor" << (int) character <<endl;
+            throw(2);
         }
         else{
             return 0;
         } 
     }
     catch(int errorCode){
-        cout << "Scanner/Lexical error.\n";
+        cout << "Scanner/Lexical error.\n" << errorCode <<endl;
         cout << "Invalid character: '" << character<< "' Line: " << lineCounter << " Column: " << indexCounter+1 <<endl;
         return 1;
     }
     
     return 0;
 }
+
 int tokenizeLine(string readLine){
     lineCounter++;
-
     vector<string> currentPhrase;
     string currentWord;
     // Makes case insensitive
@@ -54,12 +61,12 @@ int tokenizeLine(string readLine){
         else if(i==';') {
             currentPhrase.push_back(currentWord);
             currentWord.clear();
+            currentProgram.push_back(currentPhrase);
+
             return 1;
-            
         }
         // Checks if separate words
         else if(i==':'|| i == ' '){
-            
             if(currentWord.length() > 0){
                 currentPhrase.push_back(currentWord);
                 currentWord.clear();
@@ -67,7 +74,7 @@ int tokenizeLine(string readLine){
             
         }
         // Check if char is invalid
-        else if(isInvalidChar(i, indexCounter)){
+        else if(i!='-' && i!=',' && isInvalidChar(i, indexCounter)){
             cout << "            " << readLine << endl;
             cout <<"            " ; for(int i =0; i < indexCounter; i++) cout <<" "; cout<< "^" <<endl;
             return 0;
@@ -76,10 +83,22 @@ int tokenizeLine(string readLine){
         
         indexCounter++;
     }
+    currentProgram.push_back(currentPhrase);
 
-    for(auto &i : currentPhrase) cout << "Palavra: " <<i << endl;
-    cout <<"=========================" <<endl;
+    // for(auto &i : currentPhrase) cout << "Palavra: " <<i << endl;
+    // cout <<"=========================" <<endl;
     return 1;
+}
+void printProgram(){
+    for(auto &i : currentProgram){
+        cout << "Linha: ";
+        for(auto j : i){
+                cout  <<  j ;
+        }
+        cout << endl <<"=============<" <<endl;
+    }
+    cout << "Total Lines:" << lineCounter <<endl;
+
 }
 
 int analyzeCode(ifstream &inFile){
@@ -90,6 +109,7 @@ int analyzeCode(ifstream &inFile){
             cout << "Program ended with error.\n";
             return 0;
         }
+
     }
-    cout << "Total Lines:" << lineCounter <<endl;
+    return lineCounter;
 }
--- src/codeFile.asm
+++ src/codeFile.asm
@@ -4,7 +4,7 @@ N2: CONST -48
 N4: SPACE
 SECTION TEXT
 ROT: INPUT N1
-COPY N1,N4                ;comentario   sdas qualquer
+COPY N1,N4 ;comentario   qualquer
 COPY N2,N3
 COPY N3,N3
 OUTPUT N3
